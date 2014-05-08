a96eb6df2a37b68fecec9f487e7d4fdcdf5ee68d
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2014-05-07 23:16:28 -0300

    apagando imports nao usados

--- src/test/java/br/usp/each/saeg/jaguar/infra/FileUtilsTest.java
+++ src/test/java/br/usp/each/saeg/jaguar/infra/FileUtilsTest.java
@@ -1,14 +1,13 @@
 package br.usp.each.saeg.jaguar.infra;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Vector;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FileUtilsTest {
 	
+	@SuppressWarnings("rawtypes")
 	@Test
 	public void findTestClasses() throws ClassNotFoundException{
 		Class[] classes = FileUtils.findTestClasses(this.getClass());
