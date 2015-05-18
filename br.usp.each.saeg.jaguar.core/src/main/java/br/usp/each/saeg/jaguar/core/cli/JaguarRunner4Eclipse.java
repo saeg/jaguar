6f7a9845cdef95af64238d5f731dccca6341a8c1
Henrique Ribeiro <henriquelemos0@gmail.com>
2015-05-18 22:04:19 +0200

    adding runner4eclipse in jaguar.core

---
+++ br.usp.each.saeg.jaguar.core/src/main/java/br/usp/each/saeg/jaguar/core/cli/JaguarRunner4Eclipse.java
@@ -0,0 +1,73 @@
+package br.usp.each.saeg.jaguar.core.cli;
+
+import java.io.File;
+
+import org.junit.runner.JUnitCore;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import br.usp.each.saeg.jaguar.core.JaCoCoClient;
+import br.usp.each.saeg.jaguar.core.Jaguar;
+import br.usp.each.saeg.jaguar.core.heuristic.TarantulaHeuristic;
+import br.usp.each.saeg.jaguar.core.infra.FileUtils;
+import br.usp.each.saeg.jaguar.core.runner.JaguarRunListener;
+
+/**
+ * @author Henrique Ribeiro
+ * 
+ */
+public class JaguarRunner4Eclipse {
+
+	private final JUnitCore junit = new JUnitCore();
+
+	private final File projectDir;
+	private final File sourceDir;
+	private final File testDir;
+	private final Boolean isDataFlow;
+
+	public JaguarRunner4Eclipse(File projectDir, File sourceDir,
+			File testDir, Boolean isDataFlow) {
+		super();
+		this.projectDir = projectDir;
+		this.sourceDir = sourceDir;
+		this.testDir = testDir;
+		this.isDataFlow = isDataFlow;
+	}
+
+	private void run() throws Exception {
+		final Class<?>[] classes = FileUtils.findTestClasses(testDir);
+
+		final Jaguar jaguar = new Jaguar(new TarantulaHeuristic(), sourceDir, isDataFlow); //TODO create results using all Heuristics
+		final JaCoCoClient client = new JaCoCoClient(isDataFlow);
+		client.connect();
+
+		junit.addListener(new JaguarRunListener(jaguar, client));
+		junit.run(classes);
+
+		client.close();
+		jaguar.generateXML(jaguar.generateRank(), projectDir, ".jaguar\\coverage"); //TODO standard output
+	}
+
+	public static void main(String[] args){
+		final JaguarRunnerOptions options = new JaguarRunnerOptions();
+		final CmdLineParser parser = new CmdLineParser(options);
+		
+        try {
+            parser.parseArgument(args);
+            System.out.println(options.toString());
+        } catch (final CmdLineException e) {
+            System.err.println(e.getLocalizedMessage());
+            parser.printUsage(System.err);
+            System.exit(1);
+        }
+		
+		try {
+			new JaguarRunner4Eclipse(options.getProjectPath(), options.getSourcePath(), options.getTestPath(), options.getDataFlow()).run();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("End!");
+		System.exit(0);
+	}
+
+}
