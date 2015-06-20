7f84252fc75b050a998afc8a2dcd84e984a37a3f
Henrique Ribeiro <henriquelemos0@gmail.com>
2015-06-20 11:34:29 -0300

    removing unused code

--- br.usp.each.saeg.jaguar.resource/src/main/java/br/usp/each/saeg/jaguar/resource/JaguarJar.java
+++ br.usp.each.saeg.jaguar.resource/src/main/java/br/usp/each/saeg/jaguar/resource/JaguarJar.java
@@ -1,11 +1,6 @@
 package br.usp.each.saeg.jaguar.resource;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 
 /**
@@ -45,67 +40,6 @@ public class JaguarJar {
 			throw new AssertionError(ERRORMSG);
 		}
 		return stream;
-	}
-
-	/**
-	 * Extract the JaCoCo agent JAR and put it into a temporary location. This
-	 * file should be deleted on exit, but may not if the VM is terminated
-	 * 
-	 * @return Location of the Agent Jar file in the local file system. The file
-	 *         should exist and be readable.
-	 * @throws IOException
-	 *             Unable to unpack agent jar
-	 */
-	public static File extractToTempLocation() throws IOException {
-		final File agentJar = File.createTempFile("jacocoagent", ".jar");
-		agentJar.deleteOnExit();
-
-		extractTo(agentJar);
-
-		return agentJar;
-	}
-
-	/**
-	 * Extract the Jaguar JAR and put it into the specified location.
-	 * 
-	 * @param destination
-	 *            Location to write Jaguar Jar to. Must be writeable
-	 * @throws IOException
-	 *             Unable to unpack jar
-	 */
-	public static void extractTo(File destination) throws IOException {
-		InputStream inputJarStream = getResourceAsStream();
-		OutputStream outputJarStream = null;
-
-		try {
-
-			outputJarStream = new FileOutputStream(destination);
-
-			final byte[] buffer = new byte[8192];
-
-			int bytesRead;
-			while ((bytesRead = inputJarStream.read(buffer)) != -1) {
-				outputJarStream.write(buffer, 0, bytesRead);
-			}
-		} finally {
-			safeClose(inputJarStream);
-			safeClose(outputJarStream);
-		}
-	}
-
-	/**
-	 * Close a stream ignoring any error
-	 * 
-	 * @param closeable
-	 *            stream to be closed
-	 */
-	private static void safeClose(Closeable closeable) {
-		try {
-			if (closeable != null) {
-				closeable.close();
-			}
-		} catch (IOException e) {
-		}
 	}
 
 	private static final String ERRORMSG = String.format("The resource %s has not been found", RESOURCE);
