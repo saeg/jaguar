bc5a16ded6de45c0a0e598f2a935dffb0849b1c5
Henrique Ribeiro <henriquelemos0@gmail.com>
2015-07-15 21:56:38 -0300

    Returning with AllTests @RunwihtJaguar annotations.

--- br.usp.each.saeg.jaguar.core/src/test/java/br/usp/each/saeg/jaguar/AllTests.java
+++ br.usp.each.saeg.jaguar.core/src/test/java/br/usp/each/saeg/jaguar/AllTests.java
@@ -1,13 +1,13 @@
 package br.usp.each.saeg.jaguar;
 
-//import org.junit.runner.RunWith;
-//
-//import br.usp.each.saeg.jaguar.core.heuristic.OchiaiHeuristic;
-//import br.usp.each.saeg.jaguar.core.runner.JaguarRunnerHeuristic;
-//import br.usp.each.saeg.jaguar.core.runner.JaguarSuite;
+import org.junit.runner.RunWith;
 
-//@RunWith(JaguarSuite.class)
-//@JaguarRunnerHeuristic(value=OchiaiHeuristic.class, isDataflow=true)
+import br.usp.each.saeg.jaguar.core.heuristic.OchiaiHeuristic;
+import br.usp.each.saeg.jaguar.core.runner.JaguarRunnerHeuristic;
+import br.usp.each.saeg.jaguar.core.runner.JaguarSuite;
+
+@RunWith(JaguarSuite.class)
+@JaguarRunnerHeuristic(value=OchiaiHeuristic.class, isDataflow=true)
 public class AllTests {
 
 }
