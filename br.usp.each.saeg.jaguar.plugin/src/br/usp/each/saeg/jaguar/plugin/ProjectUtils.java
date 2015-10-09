588419f901d6d576bacef98c0d97a88f4625d912
Higor Amario de Souza <higoramario@gmail.com>
2015-10-08 20:36:46 -0400

    bug fixed - execution breaks when there are java files outside the correct path structure

--- br.usp.each.saeg.jaguar.plugin/src/br/usp/each/saeg/jaguar/plugin/ProjectUtils.java
+++ br.usp.each.saeg.jaguar.plugin/src/br/usp/each/saeg/jaguar/plugin/ProjectUtils.java
@@ -1,19 +1,26 @@
 package br.usp.each.saeg.jaguar.plugin;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.jdt.core.IJavaElement;
@@ -74,15 +81,77 @@ public class ProjectUtils {
     }
 
 	public static Map<String, List<IResource>> javaFilesOf(IProject project) {
-        try {
-            return visit(project, "java");
+		 final Map<String, List<IResource>> result = new HashMap<String, List<IResource>>();
+		try {
+			result.putAll(visit(project, "java"));
+			removeCopiedFiles(result);
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
             return Collections.emptyMap();
         }
     }
 	
-    public static Map<String, List<IResource>> xmlFilesOf(IProject project) {
+	/*
+	 * Remove java files which are out of the right path structure
+	 * */
+    private static void removeCopiedFiles(Map<String, List<IResource>> result) {
+    	Map<String, List<IResource>> resultWithoutCopiedFiles = new HashMap<String, List<IResource>>();
+    	Set<String> fileNames = result.keySet();
+		for(String name : fileNames){
+			for(IResource resourceFile : result.get(name)){
+				IFile file = (IFile)resourceFile;
+				if(checkPackagePath(file)){
+					if(!resultWithoutCopiedFiles.containsKey(name)){
+						resultWithoutCopiedFiles.put(name, new ArrayList<IResource>());
+					}
+					resultWithoutCopiedFiles.get(name).add(resourceFile);
+				}
+			}
+		}
+		result.clear();
+		result.putAll(resultWithoutCopiedFiles);
+	}
+    
+    /*
+     * Compares if the file path matches with package path
+     * */
+	private static boolean checkPackagePath(IFile file) {
+		try {
+			String line = "";
+			String path = file.getLocation().toString();
+			path = path.replace("/", ".");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(file.getContents()));
+			try {
+				while((line = reader.readLine()) != null){
+					if(line.contains("package") && line.contains(";")){
+						String strLine = line.substring(line.indexOf(" ")+1);
+						strLine = strLine.substring(0,strLine.indexOf(";"));
+						if(path.contains(strLine)){
+							return true;
+						}
+					}
+				}
+			} catch (IOException io) {
+				io.printStackTrace();
+			} finally {
+				if (reader != null){
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+						return false;
+					}
+				}
+			}
+		} catch (CoreException ce) {
+			ce.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+
+	public static Map<String, List<IResource>> xmlFilesOf(IProject project) {
         try {
             return visit(project, "xml");
         } catch (Exception e) {
