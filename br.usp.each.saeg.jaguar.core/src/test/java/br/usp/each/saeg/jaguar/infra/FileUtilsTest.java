cd279bf4dfbbbc0b04fba93b14be155f3e70a48f
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2016-05-12 15:40:24 -0300

    Added unit test for mehtod 'extract classes from test file'

--- br.usp.each.saeg.jaguar.core/src/test/java/br/usp/each/saeg/jaguar/infra/FileUtilsTest.java
+++ br.usp.each.saeg.jaguar.core/src/test/java/br/usp/each/saeg/jaguar/infra/FileUtilsTest.java
@@ -1,5 +1,6 @@
 package br.usp.each.saeg.jaguar.infra;
 
+import java.io.File;
 import java.util.Arrays;
 
 import org.junit.Assert;
@@ -9,10 +10,18 @@ import br.usp.each.saeg.jaguar.core.utils.FileUtils;
 
 public class FileUtilsTest {
 	
-	@SuppressWarnings("rawtypes")
+	private static final String TEST_LIST_FILE = "./src/test/resources/testClassesNames.txt";
+
 	@Test
 	public void findTestClasses() throws ClassNotFoundException{
-		Class[] classes = FileUtils.findTestClasses(this.getClass());
+		Class<?>[] classes = FileUtils.findTestClasses(this.getClass());
 		Assert.assertTrue(Arrays.asList(classes).contains(this.getClass()));
+	}
+	
+	@Test
+	public void findClassesInFile() throws ClassNotFoundException{
+		File testsListFile = new File(TEST_LIST_FILE);
+		Class<?>[] classes = FileUtils.getClassesInFile(testsListFile);
+		Assert.assertEquals(2, classes.length);
 	}
 }
