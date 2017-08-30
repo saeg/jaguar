71252b56290a6319c9a24c8b7afaf6f742c8223f
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2017-08-30 20:57:58 -0300

    Improve script to run a basic test

--- start.sh
+++ start.sh
@@ -1,15 +1,37 @@
-
 #!/bin/bash
-PROJECT_DIR="~/workspace/runtime-New_configuration/jsoup-1_3_4-1"
-JAGUAR_JAR="~/.m2/repository/br/usp/each/saeg/jaguar/br.usp.each.saeg.jaguar.core/0.0.6-SNAPSHOT/br.usp.each.saeg.jaguar.core-0.0.6-SNAPSHOT-jar-with-dependencies.jar"
-JAGUAR_MAIN_CLASS="br.usp.each.saeg.jaguar.core.cli.JaguarRunner"
 
-java -cp  "$PROJECT_DIR/target/*:$JAGUAR_JAR" \
+PROJECT_DIR="./br.usp.each.saeg.jaguar.example/"
+JAGUAR_JAR="./br.usp.each.saeg.jaguar.core/target/br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar"
+JAGUAR_MAIN_CLASS="br.usp.each.saeg.jaguar.core.cli.JaguarRunner4Eclipse"
+JACOCO_JAR="./br.usp.each.saeg.jaguar.plugin/lib/jacocoagent.jar"
+
+# CONTROL-FLOW
+#
+## JvmArgs = [-javaagent:/home/henrique/workspace/java/jaguar/br.usp.each.saeg.jaguar.plugin/lib/jacocoagent.jar=output=tcpserver,includes=*, -Xmx1024m]
+##
+## JaguarArgs = [--outputType, F, --logLevel, INFO, --projectDir, /home/henrique/workspace/runtime-EclipseApplication/example, --classesDir, /home/henrique/workspace/runtime-EclipseApplication/example/target/classes, --testsListFile, /tmp/testNames7371158783007714516.txt]
+
+java -cp $PROJECT_DIR/target/classes/ br.usp.each.saeg.jaguar.example.Max
+
+java -javaagent:$JACOCO_JAR=output=tcpserver,includes=* -cp  $PROJECT_DIR/target/classes/:$JAGUAR_JAR:$JACOCO_JAR \
 "$JAGUAR_MAIN_CLASS" \
---dataflow \
 --outputType F \
 --logLevel TRACE \
 --projectDir "$PROJECT_DIR" \
 --classesDir "$PROJECT_DIR/target/classes/" \
---testsDir "$PROJECT_DIR/target/test-classes/" \
---testsListFile "$PROJECT_DIR/testNames.txt"
+--testsListFile "$PROJECT_DIR/testListFile.txt"
+
+# DATA-FLOW
+#
+## JvmArgs = [-javaagent:/home/henrique/workspace/java/jaguar/br.usp.each.saeg.jaguar.plugin/lib/jacocoagent.jar=output=tcpserver,includes=*,dataflow=true, -Xmx1024m]
+##
+## JaguarArgs = [--dataflow, --outputType, F, --logLevel, INFO, --projectDir, /home/henrique/workspace/runtime-EclipseApplication/example, --classesDir, /home/henrique/workspace/runtime-EclipseApplication/example/target/classes, --testsListFile, /tmp/testNames4495953423924079598.txt]
+#
+#java -javaagent:$JACOCO_JAR=output=tcpserver,includes=*,dataflow=true -cp $PROJECT_DIR/target/classes/:$JAGUAR_JAR:$JACOCO_JAR" \
+#"$JAGUAR_MAIN_CLASS" \
+#--dataflow \
+#--outputType F \
+#--logLevel TRACE \
+#--projectDir "$PROJECT_DIR" \
+#--classesDir "$PROJECT_DIR/target/classes/" \
+#--testsListFile "$PROJECT_DIR/testListFile.txt"
