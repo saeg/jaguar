bb67320e6cca8f9d0160002d273233c574533ad8
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2018-04-12 08:56:18 -0300

    Change log level to variable

--- br.usp.each.saeg.jaguar.example/run.sh
+++ br.usp.each.saeg.jaguar.example/run.sh
@@ -4,13 +4,14 @@ PROJECT_DIR="./br.usp.each.saeg.jaguar.example"
 JAGUAR_JAR="./br.usp.each.saeg.jaguar.core/target/br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar"
 JAGUAR_MAIN_CLASS="br.usp.each.saeg.jaguar.core.cli.JaguarRunner4Eclipse"
 JACOCO_JAR="./br.usp.each.saeg.jaguar.plugin/lib/jacocoagent.jar"
+LOG_LEVEL="INFO" # ERROR / INFO / DEBUG / TRACE
 
 # CONTROL-FLOW
 
 java -javaagent:$JACOCO_JAR=output=tcpserver -cp $PROJECT_DIR/target/classes/:$PROJECT_DIR/target/test-classes/:$JAGUAR_JAR:$JACOCO_JAR \
 		"$JAGUAR_MAIN_CLASS" \
 			--outputType F \
-			--logLevel INFO	 \
+			--logLevel "$LOG_LEVEL" \
 			--projectDir "$PROJECT_DIR" \
 			--classesDir "$PROJECT_DIR/target/classes/" \
 			--testsListFile "$PROJECT_DIR/testListFile.txt"
@@ -21,7 +22,7 @@ java -javaagent:$JACOCO_JAR=output=tcpserver,dataflow=true -cp $PROJECT_DIR/targ
 		"$JAGUAR_MAIN_CLASS" \
 			--dataflow \
 			--outputType F \
-			--logLevel INFO \
+			--logLevel "$LOG_LEVEL" \
 			--projectDir "$PROJECT_DIR" \
 			--classesDir "$PROJECT_DIR/target/classes/" \
 			--testsListFile "$PROJECT_DIR/testListFile.txt"
