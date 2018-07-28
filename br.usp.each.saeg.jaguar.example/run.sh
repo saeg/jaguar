93dd7d68a2b0145f82ebdd387bc3280172d1d786
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2018-07-28 09:05:30 -0300

    Example script save each coverage type (data/control-flow) in a separate file

--- br.usp.each.saeg.jaguar.example/run.sh
+++ br.usp.each.saeg.jaguar.example/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 PROJECT_DIR="./br.usp.each.saeg.jaguar.example"
 JAGUAR_JAR="./br.usp.each.saeg.jaguar.core/target/br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar"
@@ -11,6 +12,7 @@ LOG_LEVEL="TRACE" # ERROR / INFO / DEBUG / TRACE
 java -javaagent:$JACOCO_JAR=output=tcpserver -cp $PROJECT_DIR/target/classes/:$PROJECT_DIR/target/test-classes/:$JAGUAR_JAR:$JACOCO_JAR \
 		"$JAGUAR_MAIN_CLASS" \
 			--outputType F \
+			--output "control-flow.xml" \
 			--logLevel "$LOG_LEVEL" \
 			--projectDir "$PROJECT_DIR" \
 			--classesDir "$PROJECT_DIR/target/classes/" \
@@ -23,6 +25,7 @@ java -javaagent:$JACOCO_JAR=output=tcpserver,dataflow=true -cp $PROJECT_DIR/targ
 		"$JAGUAR_MAIN_CLASS" \
 			--dataflow \
 			--outputType F \
+			--output "data-flow.xml" \
 			--logLevel "$LOG_LEVEL" \
 			--projectDir "$PROJECT_DIR" \
 			--classesDir "$PROJECT_DIR/target/classes/" \
